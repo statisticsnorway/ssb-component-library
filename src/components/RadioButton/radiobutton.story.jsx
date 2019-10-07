9135ba1288ec6184ab2a8e432300da38104a36e9
Nordseth, Carina <Carina.Nordseth@ssb.no>
2019-10-07 14:59:22 +0200

    Added RadioButton and RadioGroup to ssb-component

---
+++ src/components/RadioButton/radiobutton.story.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import centered from '@storybook/addon-centered/react';
+import RadioButton from './index';
+
+storiesOf('Atoms|RadioButton', module).addDecorator(centered)
+	.add('Default', () => (
+		<div>
+			<RadioButton value="item" >Item</RadioButton>
+		</div>
+	));
