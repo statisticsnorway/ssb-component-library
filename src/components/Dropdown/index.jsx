eb3720840876eeef395f69a4ff9b66512c50a593
Nordseth, Carina <Carina.Nordseth@ssb.no>
2019-10-08 15:09:39 +0200

    Added style compoment to dropdown

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import uuid from 'uuid/v4';
+import { ssbDark6, ssbGreen2, ssbGreen4, ssbWhite } from '../../style/colors'
 
 const Dropdown = ({
 	header, items, onSelect, open, placeholder, searchable, selectedItem,
@@ -43,38 +45,117 @@ const Dropdown = ({
 	}, [isOpen]);
 
 	return (
-		<div className="dropdown-component-wrapper">
-			{header && <label htmlFor={id}>{header}</label>}
-			<div className="dropdown-interactive-area" ref={node} onClick={() => setOpen(!isOpen)}>
-				<input
-					className={isOpen ? 'focused' : ''}
-					id={id}
-					onChange={e => filterItems(e)}
-					disabled={!searchable}
-					placeholder={selectedOption.title ? selectedOption.title : placeholder}
-					value={inputFieldValue}
-				/>
-				{isOpen && (
-					<ul className="list-of-options">
-						{availableOptions.map(it => (
-							<li
-								className={`option-list-element ${selectedOption.id === it.id && 'selected'}`}
-								key={it.id}
-							>
-								<option
-									onClick={() => handleSelection(it)}
-									value={it.id}
-									id={it.id}
-								>{it.title}
-								</option>
-							</li>
-						))}
-					</ul>
-				)}
+		<DropdownWrapper>
+			<div className="dropdown-component-wrapper">
+				{header && <label htmlFor={id}>{header}</label>}
+				<div className="dropdown-interactive-area" ref={node} onClick={() => setOpen(!isOpen)}>
+					<input
+						className={isOpen ? 'focused' : ''}
+						id={id}
+						onChange={e => filterItems(e)}
+						disabled={!searchable}
+						placeholder={selectedOption.title ? selectedOption.title : placeholder}
+						value={inputFieldValue}
+					/>
+					{isOpen && (
+						<ul className="list-of-options">
+							{availableOptions.map(it => (
+								<li
+									className={`option-list-element ${selectedOption.id === it.id && 'selected'}`}
+									key={it.id}
+								>
+									<option
+										onClick={() => handleSelection(it)}
+										value={it.id}
+										id={it.id}
+									>{it.title}
+									</option>
+								</li>
+							))}
+						</ul>
+					)}
+				</div>
 			</div>
-		</div>
+		</DropdownWrapper>
 	);
 };
+
+const DropdownWrapper = styled.div`
+  background: ${ssbWhite};
+  border-radius: 0;
+  display: flex;
+  flex-direction: column;
+  transition: all .25s ease-in-out;
+  
+  label {
+    margin-bottom: 5px;
+    font-size: 14px;
+  }
+  
+  .dropdown-interactive-area {
+    cursor: pointer;
+    position: relative;
+
+    input {
+      border: 1px solid ${ssbDark6};
+      cursor: pointer;
+      font-size: 16px;
+      height: 36px;
+      padding: 4px 34px 4px 10px;
+      background-color: ${ssbWhite};
+      font-family: 'Roboto', sans-serif;
+      width: 100%;
+
+      &::placeholder {
+        color: ${ssbDark6};
+      }
+
+      &:hover {
+        border: 1px solid ${ssbGreen4};
+      }
+
+      &:focus, &.focused {
+        border: 1px solid ${ssbGreen4};
+        outline: ${ssbGreen4} auto 5px;
+        outline-offset: -2px;
+      }
+    }
+
+    .list-of-options {
+      background: ${ssbWhite};
+      border: 1px solid #2b2b2b;
+      left: 0;
+      list-style: none;
+      margin: 0;
+      padding-left: 0;
+      position: absolute;
+      top: 36px;
+      z-index: 9999;
+      width: 100%;
+      font-family: 'Roboto', sans-serif;
+
+      .option-list-element {
+        cursor: pointer;
+
+        option {
+          overflow: hidden;
+          padding: 10px;
+          text-overflow: ellipsis;
+        }
+
+        &:hover {
+          background: ${ssbGreen2};
+        }
+
+        &.selected {
+          background: ${ssbGreen4};
+          color: ${ssbWhite};
+        }
+      }
+    }
+  }
+}
+`;
 
 Dropdown.defaultProps = {
 	header: '',
