f1d37e50d88a86f992263b85f2b4f44052bc65bf
kristofferund <djdifee@gmail.com>
2019-10-10 14:36:44 +0200

    Refactor paragraph

--- src/components/Paragraph/index.jsx
+++ src/components/Paragraph/index.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { openSans } from '../../style/mixins';
+import { ssbDark6 } from '../../style/colors';
 
-const Paragraph = ({ children }) => <p className="paragraph-wrapper">{ children }</p>;
+const Paragraph = ({ children }) => <Wrapper className="paragraph-wrapper">{ children }</Wrapper>;
+
+const Wrapper = styled.p`
+	${openSans};
+	color: ${ssbDark6};
+	font-size: 16px;
+	line-height: 1.7;
+`;
 
 Paragraph.propTypes = { children: PropTypes.node };
 
