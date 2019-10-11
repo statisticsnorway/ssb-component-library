5254d3b5c49b15417c1492a7b8ec159b6f2f9ecf
Nordseth, Carina <Carina.Nordseth@ssb.no>
2019-10-11 10:46:22 +0200

    Refactor Divider compoment style-component

--- src/components/Divider/index.jsx
+++ src/components/Divider/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { ssbDark2, ssbDark5 } from '../../style/colors';
 
 const Divider = ({ className, dark, light }) => (
-	<hr className={`divider-line ${dark ? 'dark' : ''}${light ? 'light' : ''} ${className}`} />
+	<DividerWrapper
+		className={className}
+		dark={dark}
+		light={light}
+	/>
 );
+
+const DividerWrapper = styled.hr`
+	border-top: 1px solid;
+	border-top-color: ${props => (props.dark ? ssbDark5 : ssbDark2)};	
+	display: block;
+  height: 2px;
+  margin: 0;
+  position: relative;
+  width: 100%;
+`;
 
 Divider.defaultProps = {
 	className: '',
