e4df82a8fcd557d2cab54ab346091dce9b87cea8
Nordseth, Carina <Carina.Nordseth@ssb.no>
2019-10-11 13:02:16 +0200

    more refactor after qa

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -2,22 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { roboto, robotoCondenced } from '../../style/mixins';
-import { ssbDark6 } from '../../style/colors';
+import { ssbDark6, ssbWhite } from '../../style/colors';
 
 const Title = ({ children, className, negative, size }) => (
-	<TitleWrapper className={`title-wrapper ${className}`}>
-		{size === 1 && <h1 className={negative ? 'negative' : ''}>{children}</h1>}
-		{size === 2 && <h2 className={negative ? 'negative' : ''}>{children}</h2>}
-		{size === 3 && <h3 className={negative ? 'negative' : ''}>{children}</h3>}
-		{size === 4 && <h4 className={negative ? 'negative' : ''}>{children}</h4>}
-		{size === 5 && <h5 className={negative ? 'negative' : ''}>{children}</h5>}
-		{size === 6 && <h6 className={negative ? 'negative' : ''}>{children}</h6>}
+	<TitleWrapper
+		className={className}
+		negative={negative}
+	>
+		{size === 1 && <h1>{children}</h1>}
+		{size === 2 && <h2>{children}</h2>}
+		{size === 3 && <h3>{children}</h3>}
+		{size === 4 && <h4>{children}</h4>}
+		{size === 5 && <h5>{children}</h5>}
+		{size === 6 && <h6>{children}</h6>}
 	</TitleWrapper>
 );
 
 const TitleWrapper = styled.div`
 	h1, h2, h3, h4, h5, h6 {
-		color: ${ssbDark6};
+		color: ${props => (props.negative ? ssbWhite : ssbDark6)};
 	}
 	
 	h1 {
