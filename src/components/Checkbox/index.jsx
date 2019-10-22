42705cf075625ed6b77308ef16609c4df7b07cdd
kristofferund <djdifee@gmail.com>
2019-10-22 13:59:13 +0200

    Create checkbox scss

--- src/components/Checkbox/index.jsx
+++ src/components/Checkbox/index.jsx
@@ -1,95 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import { ssbDark5, ssbGreen1, ssbGreen4, ssbWhite } from '../../style/colors';
-import { roboto } from '../../style/mixins';
 
 const Checkbox = ({
 	index, children, callback, selected, value, disabled,
 }) => (
-	<CheckboxWrapper
-		disabled={disabled}
-		tabIndex={index + 1}
-	>
-		<label htmlFor={value}>
-			{children}
-			<input
-				id={value}
-				checked={selected}
-				onChange={() => callback(value)}
-				type="checkbox"
-				value={value}
-			/>
-			<CheckMark
-				disabled={disabled}
-				selected={selected}
-			/>
-		</label>
-	</CheckboxWrapper>
+	<div className={`ssb-checkbox${disabled ? ' disabled' : ''}`} tabIndex={index + 1}>
+		<input
+			id={value}
+			checked={selected}
+			onChange={() => callback(value)}
+			type="checkbox"
+			value={value}
+		/>
+		<label className="checkbox-label" htmlFor={value}>{children}</label>
+	</div>
 );
-
-const CheckboxWrapper = styled.div`
-	${roboto};
-  color: ${props => (props.disabled ? '#a2baba' : '')};
-  cursor: ${props => (props.disabled ? 'default' : 'pointer')};
-  display: block;
-  min-width: 200px;
-  outline: none;
-  padding: 10px 10px 10px 45px;
-  pointer-events: ${props => (props.disabled ? 'none' : '')};
-  position: relative;
-  user-select: none;
-
-  &:hover, &:focus {
-    background: ${ssbGreen1};
-  }
-  
-  input {
-    cursor: pointer;
-    opacity: 0;
-    position: absolute;
-  }
-
-  label {
-    cursor: pointer;
-  }
-  
-`;
-
-const CheckMark = styled.span`
-    background: ${props => (props.selected ? ssbDark5 : ssbWhite)};
-    border: 1px solid;
-    border-color: ${props => (props.disabled ? '#a2baba' : ssbDark5)};
-    border-radius: 2px;
-    height: 20px;
-    left: 10px;
-    position: absolute;
-    top: 10px;
-    width: 20px;
-
-    &:after {
-      border: solid ${ssbWhite};
-      border-width: 0 3px 3px 0;
-      content: '';
-      display: ${props => (props.selected ? 'block' : 'none')};
-      height: 12px;
-      left: 6px;
-      position: absolute;
-      top: 2px;
-      transform: rotate(45deg);
-      width: 6px;
-    }
-
-    &:hover, &:focus ${CheckboxWrapper} {
-        border: 2px solid ${ssbGreen4};
-
-        &:after {
-            left: 5px;
-            top: 1px;
-        }
-    }
-
-`;
 
 Checkbox.defaultProps = {
 	callback: () => {},
