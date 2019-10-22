07974c0f3a0cbef8dff07011b2ccfe16b2a51ed1
Kristoffer Lund <kristofferrlund@gmail.com>
2019-10-22 08:30:51 +0200

    Refactor divider (#51)
    * Refactor divider component
    
    * Remove unused imports

--- src/components/Divider/index.jsx
+++ src/components/Divider/index.jsx
@@ -1,25 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import { ssbDark2, ssbDark5 } from '../../style/colors';
 
 const Divider = ({ className, dark, light }) => (
-	<DividerWrapper
-		className={className}
-		dark={dark}
-		light={light}
+	<hr
+		className={`ssb-divider ${className} ${dark ? 'type-dark' : ''} ${light ? 'type-light' : ''}`}
 	/>
 );
-
-const DividerWrapper = styled.hr`
-	border-top: 1px solid;
-	border-top-color: ${props => (props.dark ? ssbDark5 : ssbDark2)};
-	display: block;
-	height: 2px;
-	margin: 0;
-	position: relative;
-	width: 100%;
-`;
 
 Divider.defaultProps = {
 	className: '',
