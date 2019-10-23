e79e29c1b25a1d9a1860143cf37b0ec4f5352163
kristofferund <djdifee@gmail.com>
2019-10-23 12:04:59 +0200

    Make a dev build for smashing together a stylesheet for the kommunefakta team

--- src/components/Breadcrumb/index.jsx
+++ src/components/Breadcrumb/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Link from '../Link/index';
+import Link from '../Link/index.jsx';
 
 const Breadcrumb = ({ items }) => (
 	<div className="ssb-breadcrumbs">
