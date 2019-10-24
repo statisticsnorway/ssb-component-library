9e3c58406526637542e6c27ff4e73311a17719ef
kristofferund <djdifee@gmail.com>
2019-10-24 15:25:17 +0200

    Add accordion component from the old repo

---
+++ src/components/Accordion/index.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { PlusCircle, MinusCircle } from 'react-feather';
+import Title from '../Title/index.jsx';
+
+const Accordion = ({
+	children, header, openByDefault, primary, subHeader,
+}) => {
+	const [isOpen, toggleOpen] = useState(openByDefault);
+	return (
+		<div className={`ssb-accordion ${subHeader && 'with-sub-header'}`}>
+			<div
+				className={`accordion-header ${isOpen ? 'open' : 'closed'}`}
+				onClick={() => toggleOpen(!isOpen)}
+			>
+				{!isOpen && <PlusCircle className="expand-icon" size={primary ? 20 : 15} />}
+				{isOpen && <MinusCircle className="expand-icon" size={primary ? 20 : 15} />}
+				{subHeader && <Title size={5} className="sub-header no-margin">{subHeader}</Title>}
+				<Title size={5} className="header-text no-margin">{header}</Title>
+			</div>
+			<div className={`accordion-body ${isOpen ? 'open' : 'closed'}`}>
+				{isOpen && children}
+			</div>
+		</div>
+	);
+};
+
+Accordion.defaultProps = {
+	openByDefault: false,
+	primary: false,
+};
+
+Accordion.propTypes = {
+	children: PropTypes.node,
+	header: PropTypes.string,
+	openByDefault: PropTypes.bool,
+	primary: PropTypes.bool,
+	subHeader: PropTypes.string,
+};
+
+export default Accordion;
