7bd0b078225822bb1f4bb9b497cb653b828caecb
kristofferund <djdifee@gmail.com>
2019-10-24 16:11:18 +0200

    Add readme for accordion

--- src/components/NestedAccordion/index.jsx
+++ src/components/NestedAccordion/index.jsx
@@ -1,21 +1,19 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import Title from '../Title';
 import { Plus, Minus } from 'react-feather';
 
 const NestedAccordion = ({
-	children, header, openByDefault, subHeader,
+	children, header, openByDefault,
 }) => {
 	const [isOpen, toggleOpen] = useState(openByDefault);
 	return (
-		<div className={`ssb-nested-accordion mt-3 ${subHeader && 'with-sub-header'}`}>
+		<div className={`ssb-nested-accordion mt-3`}>
 			<div
 				className={`nested-accordion-header ${isOpen ? 'open' : 'closed'}`}
 				onClick={() => toggleOpen(!isOpen)}
 			>
 				{!isOpen && <Plus className="expand-icon" size="10" />}
 				{isOpen && <Minus className="expand-icon" size="10" />}
-				{subHeader && <Title size={5} className="sub-header">{subHeader}</Title>}
 				<span className="header-text">{header}</span>
 			</div>
 			<div className={`nested-accordion-body ${isOpen ? 'open' : 'closed'}`}>
@@ -33,7 +31,6 @@ NestedAccordion.propTypes = {
 	children: PropTypes.node,
 	header: PropTypes.string,
 	openByDefault: PropTypes.bool,
-	subHeader: PropTypes.string,
 };
 
 export default NestedAccordion;
