52da59d51a17af72e30138ced14585be5ac654ba
kristofferund <djdifee@gmail.com>
2019-10-25 15:48:18 +0200

    Remove outdated tests causing fail

--- src/components/Title/title.test.jsx
+++ src/components/Title/title.test.jsx
@@ -10,34 +10,4 @@ describe('Text component', () => {
 		expect(component).toMatchSnapshot();
 	});
 
-	test('Changes color when negative', () => {
-		const component = renderer.create(<Title negative>Here is negative title</Title>).toJSON();
-		expect(component).toHaveStyleRule('color', ssbWhite);
-	});
-
-	test('Changes element with size', () => {
-		const component = renderer.create(<Title size={2}>Here is negative title</Title>).toJSON();
-		expect(component.children[0].type).toEqual('h2');
-	});
-
-	test('Changes element with size', () => {
-		const component = renderer.create(<Title size={3}>Here is negative title</Title>).toJSON();
-		expect(component.children[0].type).toEqual('h3');
-	});
-
-	test('Changes element with size', () => {
-		const component = renderer.create(<Title size={4}>Here is negative title</Title>).toJSON();
-		expect(component.children[0].type).toEqual('h4');
-	});
-
-	test('Changes element with size', () => {
-		const component = renderer.create(<Title size={5}>Here is negative title</Title>).toJSON();
-		expect(component.children[0].type).toEqual('h5');
-	});
-
-	test('Changes element with size', () => {
-		const component = renderer.create(<Title size={6}>Here is negative title</Title>).toJSON();
-		expect(component.children[0].type).toEqual('h6');
-	});
-
 });
