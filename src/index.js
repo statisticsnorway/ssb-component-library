bbc48d346385718018dd0569ae69135c2229c6b8
kristofferund <djdifee@gmail.com>
2019-10-28 12:29:54 +0100

    Add missing components to global export

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
 import './main.scss';
 import './style/globalStyle';
 
+export { default as Accordion } from './components/Accordion';
+export { default as BlockContent } from './components/BlockContent';
 export { default as Breadcrumb } from './components/Breadcrumb';
 export { default as Button } from './components/Button';
 export { default as Checkbox } from './components/Checkbox';
 export { default as CheckboxGroup } from './components/CheckboxGroup';
+export { default as Dialog } from './components/Dialog';
 export { default as Divider } from './components/Divider';
 export { default as Dropdown } from './components/Dropdown';
 export { default as Input } from './components/Input';
 export { default as KeyFigures } from './components/KeyFigures';
 export { default as LeadParagraph } from './components/LeadParagraph';
 export { default as Link } from './components/Link';
+export { default as NestedAccordion } from './components/NestedAccordion';
 export { default as Number } from './components/Number';
 export { default as Pagination } from './components/Pagination';
 export { default as Paragraph } from './components/Paragraph';
+export { default as RadioButton } from './components/RadioButton';
+export { default as RadioGroup } from './components/RadioGroup';
 export { default as Tabs } from './components/Tabs';
 export { default as Text } from './components/Text';
 export { default as Title } from './components/Title';
 export { default as WordExplanation } from './components/WordExplanation';
-export { default as RadioButton } from './components/RadioButton';
-export { default as RadioGroup } from './components/RadioGroup';
