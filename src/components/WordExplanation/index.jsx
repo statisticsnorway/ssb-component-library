aa8297da724df23e56174d2a31876ff3fec86388
kristofferund <djdifee@gmail.com>
2019-10-30 10:49:04 +0100

    Refactor wordExplanation to be able to use it with only css and html

--- src/components/WordExplanation/index.jsx
+++ src/components/WordExplanation/index.jsx
@@ -8,7 +8,6 @@ const WordExplanation = ({
 	const node = useRef();
 	const infoContainer = useRef();
 	const [open, setOpen] = useState(false);
-	const [position, setPosition] = useState({ x: 0, y: 0 });
 
 	const handleClickOutside = e => {
 		if (node.current.contains(e.target)) {
@@ -17,27 +16,7 @@ const WordExplanation = ({
 		setOpen(false);
 	};
 
-	const calculatePosition = (x, y, width) => {
-		const topMargin = 30;
-		const infoBoxWidth = 270;
-		if (open && window.innerWidth <= 768) {
-			// Center for mobile devices
-			setPosition({ x: (window.innerWidth - infoContainer.current.clientWidth) / 2, y: y + topMargin });
-		}
-		else {
-			if (x + infoBoxWidth > window.innerWidth) {
-				// Detect if box will go outside of screen
-				const adjustment = x + infoBoxWidth - window.innerWidth;
-				setPosition({ x: x - adjustment, y: y + topMargin });
-			} else {
-				const adjustment = (infoBoxWidth / 2) - (width / 2);
-				setPosition({ x: x - adjustment, y: y + topMargin });
-			}
-		}
-	};
-
 	useEffect(() => {
-		calculatePosition(node.current.offsetLeft, node.current.offsetTop, node.current.offsetWidth);
 		if (open) {
 			document.addEventListener('mousedown', handleClickOutside);
 		} else {
@@ -50,16 +29,10 @@ const WordExplanation = ({
 	}, [open]);
 
 	return (
-		<span ref={node} onClick={() => setOpen(!open)} className="ssb-word-explanation">
+		<div ref={node} onClick={() => setOpen(!open)} className="ssb-word-explanation">
 			{children}<FileText size="12" />
 			<div className="animate-background" />
-			{open && (
-				<div className="ssb-we-popup" ref={infoContainer} style={{ top: position.y, left: position.x }}>
-					<div className="ssb-we-arrow">
-						<svg width="16" height="16">
-							<rect width="16" height="16" rotate="45deg" />
-						</svg>
-					</div>
+				<div className={`ssb-we-popup ${open && 'open'}`} ref={infoContainer}>
 					<div className="content-box">
 						<span className="info-text">{explanation}</span>
 						<div className="ssb-we-closing">
@@ -67,8 +40,7 @@ const WordExplanation = ({
 						</div>
 					</div>
 				</div>
-			)}
-		</span>
+		</div>
 	);
 };
 
