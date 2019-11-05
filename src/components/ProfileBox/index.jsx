c4cbdb67916c12d44508fc18440a1383f68cca97
Kristoffer Lund <kristofferrlund@gmail.com>
2019-11-05 10:56:06 +0100

    Implement the updated profile box design (#94)
    * Implement the updated profile box design
    
    * Fix README

--- src/components/ProfileBox/index.jsx
+++ src/components/ProfileBox/index.jsx
@@ -1,23 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Link from '../Link';
-import Paragraph from '../Paragraph';
+import { Download } from 'react-feather';
 
 const ProfileBox = ({
-	link, text, title,
+	centered, children, fileLocation, onClick,
 }) => (
 	<div className="ssb-profile-box">
-		<Link href={link} linkType="header">{title}</Link>
-		<Paragraph>{text}</Paragraph>
+		<a className={`profile-content${centered ? ' centered' : ''}`} onClick={() => onClick}>
+			{ children }
+		</a>
+		{fileLocation && (
+			<a download href={fileLocation} className="download-section">
+				<Download className="download-icon" size={20} />
+				<span>Last ned</span>
+			</a>
+		)}
 	</div>
 );
 
-ProfileBox.defaultProps = {};
+ProfileBox.defaultProps = {
+	centered: false,
+	onClick: () => {},
+};
 
 ProfileBox.propTypes = {
-	link: PropTypes.string,
-	text: PropTypes.string,
-	title: PropTypes.string,
+	centered: PropTypes.bool,
+	children: PropTypes.node.isRequired,
+	fileLocation: PropTypes.string,
+	onClick: PropTypes.func,
 };
 
 export default ProfileBox;
--- src/components/ProfileBox/profilebox.story.jsx
+++ src/components/ProfileBox/profilebox.story.jsx
@@ -2,15 +2,33 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import centered from '@storybook/addon-centered/react';
 import ProfileBox from './index';
+import Button from '../Button';
+import Paragraph from '../Paragraph';
+import Title from '../Title';
 
 storiesOf('ProfileBox', module).addDecorator(centered)
 	.add('Default', () => (
-		<ProfileBox link=" " text="Explaining text about something." title="This is a title link" />
+		<div style={{width: '300px'}}>
+			<ProfileBox>
+				<Title size={2}>Look at this</Title>
+				<Paragraph>Explain something about something with something clever.</Paragraph>
+				<Button primary>Handling</Button>
+			</ProfileBox>
+		</div>
 	))
 	.add('Three boxes', () => (
 		<div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 250px)', gridColumnGap: '20px' }}>
-			<ProfileBox link=" " text="Explaining text about something." title="This is a title link Box 1" />
-			<ProfileBox link=" " text="Explaining text about something." title="This is a title link Box 2" />
-			<ProfileBox link=" " text="Explaining text about something." title="This is a title link Box 3" />
+			<ProfileBox>
+				<Title size={2}>Look at this</Title>
+				<Paragraph>Explain something about something with something clever.</Paragraph>
+			</ProfileBox>
+			<ProfileBox centered>
+				<Title size={2}>Look at this</Title>
+				<Paragraph>This box has the <code>centered</code> prop set to true</Paragraph>
+			</ProfileBox>
+			<ProfileBox fileLocation="./not_a_file.md">
+				<Title size={2}>Look at this</Title>
+				<Paragraph>Explain something about something with something clever.</Paragraph>
+			</ProfileBox>
 		</div>
 	));
