7c5c325008e85721f373a2b259fda39c917aa76d
kristofferund <djdifee@gmail.com>
2019-11-08 09:04:04 +0100

    Enforce max-width on text elements

--- src/components/Paragraph/paragraph.story.jsx
+++ src/components/Paragraph/paragraph.story.jsx
@@ -5,7 +5,13 @@ import Paragraph from './index';
 
 storiesOf('Paragraph', module).addDecorator(centered)
 	.add('Standard', () => (
-		<Paragraph>
-			A paragraph is useful for wrapping text.
-		</Paragraph>
+		<div>
+			<Paragraph>
+				A paragraph is useful for wrapping text.
+			</Paragraph>
+			<Paragraph>
+				A long text will have to be limited to 580px in width to make sure that it is easy to read and that the reader will be able to find
+				the next line easily. If the lines are too long it is easy to loose track and it will become very annoying.
+			</Paragraph>
+		</div>
 	));
