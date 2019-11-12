aa177737c34e6f09b8a6b6bf9220043a5225b691
Kristoffer Lund <kristofferrlund@gmail.com>
2019-11-12 15:59:51 +0100

    Word explanation on KeyFigures (#113)
    * Add custom icon
    
    * Add optional word explanation to key figures
    
    * Update docs
    
    * Remove h4 subtitle due to UU

--- src/components/KeyFigures/index.jsx
+++ src/components/KeyFigures/index.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Number from '../Number';
-import Title from '../Title';
+import WordExplanation from '../WordExplanation';
 
-const KeyFigures = ({	icon, number, numberDescription, size, title, time }) => (
+const KeyFigures = ({	icon, number, numberDescription, size, title, time, wordExplanation }) => (
 	<div className="ssb-key-figures">
 		<div className={`kf-icon ${size}`}>{icon}</div>
 		<div>
-			<Title className="kf-title" size={4}>{title}</Title>
+			{wordExplanation ?
+				<WordExplanation explanation={'fsf'}>
+					<span className="kf-title">{title}</span>
+				</WordExplanation>
+				: <span className="kf-title">{title}</span>
+			}
 			<div className="kf-time">{time}</div>
 			<Number size={size} number={number} />
-			<Title className="kf-title" size={4}>{numberDescription}</Title>
+			<span className="kf-title subtitle">{numberDescription}</span>
 		</div>
 	</div>
 );
@@ -30,6 +35,7 @@ KeyFigures.propTypes = {
 		PropTypes.string,
 		PropTypes.number,
 	]),
+	wordExplanation: PropTypes.string,
 };
 
 export default KeyFigures;
