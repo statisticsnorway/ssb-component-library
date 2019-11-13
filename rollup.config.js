5f4f434d9681069c19f4cec78ae1b54bee594f38
kristofferund <djdifee@gmail.com>
2019-11-13 14:45:13 +0100

    Remove some options in the rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,16 +3,15 @@ import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import replace from 'rollup-plugin-replace';
 import scss from 'rollup-plugin-scss';
-import svg from 'rollup-plugin-svg';
+import url from 'rollup-plugin-url';
 import { uglify } from 'rollup-plugin-uglify';
 
 const NODE_ENV = process.env.ENV || 'development';
-const outputFile = NODE_ENV === 'production' ? './lib/prod.js' : './lib/dev.js';
 
 export default [{
 	input: './src/index.js',
 	output: {
-		file: outputFile,
+		file: 'lib/bundle.cjs.js',
 		format: 'cjs',
 	},
 	external: [
@@ -22,9 +21,13 @@ export default [{
 	],
 	plugins: [
 		scss({
-			output: NODE_ENV === 'development' ? 'bundled-styles.scss' : 'lib/bundle.css',
+			output: 'lib/bundle.css',
 		}),
-		svg(),
+		url({
+			destDir: './lib',
+			limit: 10 * 1024,
+			include: ['**/*.svg'],
+		}),
 		resolve({ extension: ['.jsx', '.js'] }),
 		replace({
 			'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
