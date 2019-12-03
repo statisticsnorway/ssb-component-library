5f486bf7430caafa6f88c337d093d7e6096c807e
kristofferund <djdifee@gmail.com>
2019-12-03 11:05:03 +0100

    Add image properties. TODO: fix sizing issue

--- src/components/Card/card.story.jsx
+++ src/components/Card/card.story.jsx
@@ -5,15 +5,30 @@ import Card from './index';
 import Button from '../Button';
 import Paragraph from '../Paragraph';
 import Title from '../Title';
+import testImage from '../../media/16x9.png'
 
 storiesOf('Card', module).addDecorator(centered)
 	.add('Default', () => (
-		<div style={{ width: '300px' }}>
-			<Card>
+		<Card imagePlacement="left">
+			<Title size={2}>Look at this</Title>
+			<Paragraph>Explain something about something with something clever.</Paragraph>
+			<Button primary>Handling</Button>
+		</Card>
+	))
+	.add('With images', () => (
+		<div>
+			<Card imagePlacement="left" image={<img height="180px" src={testImage} alt="testImage" />}>
 				<Title size={2}>Look at this</Title>
 				<Paragraph>Explain something about something with something clever.</Paragraph>
 				<Button primary>Handling</Button>
 			</Card>
+			<div>
+				<Card imagePlacement="top" image={<img height="180px" src={testImage} alt="testImage" />}>
+					<Title size={2}>Look at this</Title>
+					<Paragraph>Explain something about something with something clever.</Paragraph>
+					<Button primary>Handling</Button>
+				</Card>
+			</div>
 		</div>
 	))
 	.add('Three boxes', () => (
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,11 +3,17 @@ import PropTypes from 'prop-types';
 import { Download } from 'react-feather';
 
 const Card = ({
-	centered, children, fileLocation, onClick,
+	centered, children, fileLocation, image, imagePlacement, onClick,
 }) => (
 	<div className="ssb-card">
-		<button className={`card-content${centered ? ' centered' : ''}`} onClick={() => onClick}>
-			{ children }
+		<button
+			className={`clickable ${imagePlacement === 'left' ? 'left-orientation' : 'top-orientation'}`}
+			onClick={() => onClick}
+		>
+			{image}
+			<div className={`card-content${centered ? ' centered' : ''}`}>
+				{ children }
+			</div>
 		</button>
 		{fileLocation && (
 			<a download href={fileLocation} className="download-section">
@@ -20,6 +26,7 @@ const Card = ({
 
 Card.defaultProps = {
 	centered: false,
+	imagePlacement: 'top',
 	onClick: () => {},
 };
 
@@ -27,6 +34,8 @@ Card.propTypes = {
 	centered: PropTypes.bool,
 	children: PropTypes.node.isRequired,
 	fileLocation: PropTypes.string,
+	image: PropTypes.element,
+	imagePlacement: PropTypes.oneOf(['left', 'top']),
 	onClick: PropTypes.func,
 };
 
