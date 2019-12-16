34114fea5e7987df8b3051fb7511eed356d603aa
Nordseth, Carina <Carina.Nordseth@ssb.no>
2019-12-16 14:11:50 +0100

    Addet mobile story StickyMenu

--- src/components/StickyMenu/stickyMenu.story.jsx
+++ src/components/StickyMenu/stickyMenu.story.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import centered from '@storybook/addon-centered/react';
-import { ChevronDown } from 'react-feather';
+import { ChevronDown, Map, Search } from 'react-feather';
 import StickyMenu from './index';
 import Input from '../Input';
 import Text from '../Text';
+import Divider from '../Divider';
 
 storiesOf('StickyMenu', module).addDecorator(centered)
 	.add('Default', () => (
@@ -23,6 +24,24 @@ storiesOf('StickyMenu', module).addDecorator(centered)
 						<div style={{ marginLeft: '30px' }} className="choose-chart">Velg i kart</div>
 						<ChevronDown size={18} className="chevron-down-icon" />
 					</div>
+				</div>
+			</StickyMenu>
+		</div>
+	))
+	.add('Mobile', () => (
+		<div style={{ width: '360px' }}>
+			<StickyMenu>
+				<div style={{ alignItems: 'center', display: 'flex', justifyContent: 'space-between', width: 'auto', paddingLeft: '20px', paddingRight: '20px', paddingBottom: '16px' }}>
+					<div style={{ fontSize: '18px', fontWeight: 'bold' }}>Stordal (Møre og Romsdal)</div>
+					<div style={{ display: 'flex' }}>
+						<Search size={18} className="chevron-down-icon" style={{ marginRight: '4px', backgroundColor: '#00824d', color: '#ffffff', padding: '4px' }} />
+						<Map size={18} className="chevron-down-icon" style={{ padding: '4px' }} />
+					</div>
+				</div>
+				<Divider light />
+				<div style={{ width: 'auto', paddingTop: '12px', paddingLeft: '20px', paddingRight: '20px' }}>
+					<div style={{ fontSize: '16px', fontWeight: 'bold', paddingBottom: '8px' }}>Bytt kommune:</div>
+					<Input id="kommune" ariaLabel="Søk på kommune" searchField placeholder="Søk på kommune" />
 				</div>
 			</StickyMenu>
 		</div>
