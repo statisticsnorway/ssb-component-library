357c871dba9345e48352fa720e72d46becdb51c4
kristofferlund <kristofferrlund@gmail.com>
2019-12-17 10:34:33 +0100

    Implement a working example of the glossary component with react-popper

--- src/components/Glossary/index.jsx
+++ src/components/Glossary/index.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState, useRef } from 'react';
 import PropTypes from 'prop-types';
+import { Manager, Reference, Popper } from 'react-popper';
 import { BookOpen, XCircle } from 'react-feather';
 
 const Glossary = ({ explanation, children }) => {
 	const node = useRef();
-	const infoContainer = useRef();
 	const [open, setOpen] = useState(false);
 
 	const handleClickOutside = e => {
@@ -27,22 +27,41 @@ const Glossary = ({ explanation, children }) => {
 	}, [open]);
 
 	return (
-		<button ref={node} onClick={() => setOpen(!open)} className="ssb-glossary">
-			<div className="glossary-text-wrap">{children}</div>
-			<BookOpen size={12} className="glossary-logo" />
-			<div className="glossary-animate-background" />
-			{open && (
-				<div className={`glossary-popup${open ? ' open' : ''}`} ref={infoContainer}>
-					<div className="content-box">
-						<span className="info-text">{explanation}</span>
-						<div className="ssb-glossary-closing">
-							<XCircle size={16} className="icon" /><span>Lukk</span>
-						</div>
-					</div>
-				</div>
-			)}
+		<div className="ssb-glossary">
+			<Manager>
+				<button ref={node} onClick={() => setOpen(!open)} className="glossary-button">
+					<Reference>
+						{({ ref }) => (
+							<div ref={ref}>
+								<div className="glossary-text-wrap">{children}</div>
+								<BookOpen size={12} className="glossary-logo" />
+								<div className="glossary-animate-background" />
+							</div>
+						)}
+					</Reference>
+				</button>
+				{open && (
+					<Popper placement="bottom" modifiers={{ preventOverflow: { escapeWithReference: false } }}>
+						{({ ref, style, placement, outOfBoundaries }) => (
+							<div
+								className={`glossary-popup${open ? ' open' : ''}`}
+								ref={ref}
+								style={{ opacity: outOfBoundaries ? 0 : 1, ...style }}
+								data-placement={placement}
+							>
+								<div className="content-box">
+									<span className="info-text">{explanation}</span>
+									<div className="ssb-glossary-closing">
+										<XCircle size={16} className="icon" /><span>Lukk</span>
+									</div>
+								</div>
+							</div>
+						)}
 
-		</button>
+					</Popper>
+				)}
+			</Manager>
+		</div>
 	);
 };
 
