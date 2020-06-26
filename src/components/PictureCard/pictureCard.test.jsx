515f6898aafcfa88e74d361f4fa8bd7f6fbc56ae
prabu-ssb <prabakar.venkataraman@ssb.no>
2020-06-26 12:25:12 +0200

    updated test/snapshots

--- src/components/PictureCard/pictureCard.test.jsx
+++ src/components/PictureCard/pictureCard.test.jsx
@@ -5,15 +5,17 @@ import cardImage from './cards_bilde.jpg';
 
 describe('ImageLink component', () => {
 	test('Matches the snapshot', () => {
-		const wrapper = shallow(<PictureCard image={<img src={cardImage} alt="image" />} onClick={() => {}} type="Type" title="Tittel" />);
+		const wrapper = shallow(
+			<PictureCard imageSrc={cardImage} onClick={() => {}} type="Type" altText="img alt" title="Tittel" />
+		);
 		expect(wrapper).toMatchSnapshot();
 	});
 	test('Sets default orientation className', () => {
-		const wrapper = shallow(<PictureCard image={<img src={cardImage} alt="image" />} onClick={() => {}} type="Type" title="Tittel" />);
+		const wrapper = shallow(<PictureCard imageSrc={cardImage} altText="alt img" onClick={() => {}} type="Type" title="Tittel" />);
 		expect(wrapper.hasClass('vertical')).toEqual(true);
 	});
 	test('Switches className on orientation change', () => {
-		const wrapper = shallow(<PictureCard orientation="horizontal" image={<img src={cardImage} alt="image" />} onClick={() => {}} type="Type" title="Tittel" />);
+		const wrapper = shallow(<PictureCard orientation="horizontal" imageSrc={cardImage} altText="alt img" onClick={() => {}} type="Type" title="Tittel" />);
 		expect(wrapper.hasClass('horizontal')).toEqual(true);
 	});
 });
