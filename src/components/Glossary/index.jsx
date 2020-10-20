32a1bb814caa61121665df2f895536314552ea71
erik chrisander gustavsen <e.c.gustavsen@gmail.com>
2020-10-20 09:38:35 +0200

    rewrite of component using another tooltip library based on react-popper

--- src/components/Glossary/index.jsx
+++ src/components/Glossary/index.jsx
@@ -1,78 +1,69 @@
-import React, { useEffect, useState, useRef } from 'react';
+// eslint-disable jsx-props-no-spreading
+
+import React from 'react';
 import PropTypes from 'prop-types';
-import { Manager, Reference, Popper } from 'react-popper';
+import TooltipTrigger from 'react-popper-tooltip';
 import { BookOpen, XCircle } from 'react-feather';
 
-const Glossary = ({ explanation, children, className, closeText }) => {
-	const node = useRef();
-	const [open, setOpen] = useState(false);
-
-	const handleClickOutside = e => {
-		if (node.current.contains(e.target)) {
-			return;
-		}
-		setOpen(false);
-	};
-
-	useEffect(() => {
-		if (open) {
-			document.addEventListener('mousedown', handleClickOutside);
-		} else {
-			document.removeEventListener('mousedown', handleClickOutside);
-		}
-
-		return () => {
-			document.removeEventListener('mousedown', handleClickOutside);
-		};
-	}, [open]);
-
-	return (
-		<div className="ssb-glossary">
-			<Manager>
-				<button ref={node} onClick={() => setOpen(!open)} className="glossary-button">
-					<Reference>
-						{({ ref }) => (
-							<div ref={ref}>
-								<div className="glossary-text-wrap">{children}</div>
-								<BookOpen size={12} className="glossary-logo" />
-								<div className="glossary-animate-background" />
-							</div>
-						)}
-					</Reference>
+const Glossary = ({ children, tooltip, hideArrow, className, explanation, closeText, ...props }) => (
+	<div className="ssb-glossary">
+		<TooltipTrigger
+			{...props}
+			tooltip={({
+				arrowRef,
+				tooltipRef,
+				getArrowProps,
+				getTooltipProps,
+				placement,
+			}) => (
+				<div
+					{...getTooltipProps({
+						ref: tooltipRef,
+					})}
+					className="ssb-glossary-popup"
+				>
+					{!hideArrow && (
+						<div
+							{...getArrowProps({
+								ref: arrowRef,
+								'data-placement': placement,
+							})}
+						/>
+					)}
+					<div className="content-box">
+						<span className="info-text">{explanation}</span>
+					</div>
+				</div>
+			)}
+		>
+			{({ getTriggerProps, triggerRef }) => (
+				<button
+					{...getTriggerProps({
+						ref: triggerRef,
+					})}
+					className="glossary-button"
+				>
+					<div className="glossary-text-wrap">{children}</div>
+					<BookOpen size={12} className="glossary-logo" />
+					<div className="glossary-animate-background" />
 				</button>
-				{open && (
-					<Popper placement="bottom" modifiers={{ preventOverflow: { escapeWithReference: false } }}>
-						{({ ref, style, placement, outOfBoundaries }) => (
-							<div
-								className={`glossary-popup${open ? ' open' : ''}`}
-								ref={ref}
-								style={{ opacity: outOfBoundaries ? 0 : 1, ...style }}
-								data-placement={placement}
-							>
-								<div className="content-box">
-									<span className="info-text">{explanation}</span>
-									<div className="ssb-glossary-closing">
-										<XCircle size={16} className="icon" /><span>Lukk</span>
-									</div>
-								</div>
-							</div>
-						)}
-
-					</Popper>
-				)}
-			</Manager>
-		</div>
-	);
-};
+			)}
+		</TooltipTrigger>
+	</div>
+);
 
 Glossary.defaultProps = {
 	closeText: 'Lukk',
+	placement: 'bottom',
+	trigger: 'click',
 };
 
 Glossary.propTypes = {
 	children: PropTypes.node,
 	className: PropTypes.string,
 	closeText: PropTypes.string,
+	placement: PropTypes.string,
+	trigger: PropTypes.string,
 	explanation: PropTypes.string.isRequired,
 };
 
