c3317ef4aaaa86492cfc949b67758269e786f383
erik chrisander gustavsen <e.c.gustavsen@gmail.com>
2020-11-18 14:03:58 +0100

    Bumped version number and removed min-width

--- src/components/CategoryLink/categorylink.story.jsx
+++ src/components/CategoryLink/categorylink.story.jsx
@@ -5,7 +5,12 @@ import CategoryLink from './index';
 
 storiesOf('CategoryLink', module).addDecorator(centered)
 	.add('Default', () => (
-		<div>
-			<CategoryLink href=" " titleText="Category link with description" subText="Description goes here" />
+		<div style={{ width: '1200px', display: 'flex', justifyContent: 'center' }}>
+			<div style={{ width: '580px', marginRight: '20px' }}>
+				<CategoryLink href=" " titleText="Category link with description" subText="Description goes here" />
+			</div>
+			<div style={{ width: '580px' }}>
+				<CategoryLink href=" " titleText="Category link with description" subText="Description goes here" />
+			</div>
 		</div>
 	));
