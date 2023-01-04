8eddf22f36f1196fe189c95e28a9451558f22cc0
Kyle Danny <codingkyle92@gmail.com>
2023-01-04 14:52:13 +0100

    removed unsused styles

--- src/components/ButtonTertiary/buttonTertiary.story.jsx
+++ src/components/ButtonTertiary/buttonTertiary.story.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import ButtonTertiary from './index';
+import Card from '../Card';
 
 storiesOf('ButtonTertiary', module)
 	.add('Tertiary', () => (
-		<div style={{ width: '900px', display: 'flex' }}>
+		<div style={{ width: '900px', height: '200px', display: 'flex', flexDirection: 'inline' }}>
 			<ButtonTertiary className="mr-4" header="Avsluttet statistikk">
-				<div>
-					<p>Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum </p>
-					<p>Ipsum</p>
-				</div>
+				<Card title="Tittel" href=" ">
+					<p>Explain something about something with something clever.</p>
+				</Card>
+				<Card title="Tittel" href=" ">
+					<p>Explain something about something with something clever.</p>
+				</Card>
+				<Card title="Tittel" href=" ">
+					<p>Explain something about something with something clever.</p>
+				</Card>
 			</ButtonTertiary>
 		</div>
 	));
