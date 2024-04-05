bdd76d815c42ac95832ae33914de5bfb71f32123
Glenruben Larsen <glenruben@gmail.com>
2024-04-05 13:44:57 +0200

    Upgrade storybook to version 8

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,53 +1,49 @@
 const path = require('path');
 
 module.exports = {
-    stories: ['../**/*.story.jsx'],
-    addons: [
-        '@storybook/addon-links'
-    ],
-    staticDirs: ['../src/media'],
+  stories: ['../**/*.story.jsx'],
 
-    webpackFinal: config => {
-        const fileLoaderRule = config.module.rules.find(
-            (rule) => rule.test && rule.test.test(".svg")
-        );
-        fileLoaderRule.exclude = /\.svg$/;
-        config.module.rules.push(
-            {
-				include: path.resolve(__dirname, '../'),
-				test: /\.(s*)css$/i,
-                sideEffects: true,
-				use: [
-					'style-loader',
-					'css-loader',
-                    'sass-loader'
-				],
-			},
-            {
-                test: /\.(png|jpg|gif|woff|ttf)$/,
-				use: [
-					'file-loader',
-				],
-			},
-			{
-				test: /\.svg$/,
-				use: ['@svgr/webpack'],
-			},
-        );
-        return config;
-    },
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-webpack5-compiler-babel',
+  ],
 
-    resolve: {
-		extensions: ['.jsx', '.js'],
-	},
+  staticDirs: ['../src/media'],
 
-    features: {
-        postcss: false,
-    },
+  webpackFinal: (config) => {
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => rule.test && rule.test.test('.svg')
+    );
+    fileLoaderRule.exclude = /\.svg$/;
+    config.module.rules.push(
+      {
+        include: path.resolve(__dirname, '../'),
+        test: /\.(s*)css$/i,
+        sideEffects: true,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|woff|ttf)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      }
+    );
+    return config;
+  },
 
-    framework: {
-        name: '@storybook/react-webpack5',
-        options: {}
-    },
+  resolve: {
+    extensions: ['.jsx', '.js'],
+  },
+
+  features: {
+    postcss: false,
+  },
+
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
 };
-
