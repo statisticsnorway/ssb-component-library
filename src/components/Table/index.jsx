1654fecbcbc9edda1b87591934f2b384d9f61731
johnnadeluy <johnnadel.salita@gmail.com>
2024-05-15 14:44:17 +0200

    Update examples and styling; indentation, text-alignment etc

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -4,61 +4,22 @@ import PropTypes from 'prop-types';
 // TODO:
 // * React hooks e.g. useRef
 // * WAI-ARIA
+// * Toggle-able statistics table (including sum and level)
 
 const Table = ({ tableClass, caption, thead, tbody, tfoot }) => (
 	<div className="ssb-table">
 		<table className={`${tableClass ?? ''}`}>
-			{caption ? (
+			{caption && (
 				<caption>
 					<div className="caption-text-wrapper">
 						{caption}
 					</div>
+					<div className="scroll-icon-wrapper" />
 				</caption>
-			) : null}
-			{thead?.length ? (
-				<thead>
-					{thead.map(() => (
-						<>
-							<tr>
-								<th rowSpan={2} />
-								<th colSpan={2}>Header 1</th>
-							</tr>
-							<tr>
-								<th>Header 2</th>
-								<th>Header 3
-								</th>
-							</tr>
-						</>
-					))}
-				</thead>
-			) : null}
-			{tbody?.length ? (
-				<tbody>
-					{tbody.map(() => (
-						<>
-							<tr>
-								<td>Body 1</td>
-								<td>Body 2</td>
-								<td>Body 3</td>
-							</tr>
-							<tr>
-								<td>Body 4</td>
-								<td>Body 5</td>
-								<td>Body 6</td>
-							</tr>
-						</>
-					))}
-				</tbody>
-			) : null}
-			{tfoot?.length ? (
-				<tfoot>
-					{tfoot.map(() => (
-						<tr>
-							<td colSpan="100%"><sup>1</sup>&nbsp;Footer 1</td>
-						</tr>
-					))}
-				</tfoot>
-			) : null}
+			)}
+			{thead && <thead>{thead}</thead>}
+			{tbody && <tbody>{tbody}</tbody>}
+			{tfoot && <tfoot>{tfoot}</tfoot>}
 		</table>
 	</div>
 );
@@ -70,9 +31,9 @@ Table.defaultProps = {
 Table.propTypes = {
 	tableClass: PropTypes.string,
 	caption: PropTypes.string,
-	thead: PropTypes.array,
-	tbody: PropTypes.array,
-	tfoot: PropTypes.array,
+	thead: PropTypes.element,
+	tbody: PropTypes.element,
+	tfoot: PropTypes.element,
 };
 
 export default Table;
--- src/components/Table/table.story.jsx
+++ src/components/Table/table.story.jsx
@@ -5,17 +5,165 @@ export default {
 	title: 'Table',
 };
 
-export const TableExample = () => (
+export const Default = () => (
 	<div style={{ width: '1400px' }}>
 		<Table
-			caption="Tabellens tittel skal inn her, ofte ganske lang og over i hvert fall to linjer. Kanskje lengre. Ofte en del lengre og kan være over flere linjer"
-			thead={['1']}
-			tbody={['2']}
-			tfoot={['3']}
+			caption="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non pretium felis. Aenean eu ipsum in magna auctor porta. Donec vestibulum nulla vel laoreet blandit."
+			thead={(
+				<tr>
+					{/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+					<th>&nbsp;</th>
+					<th>Header</th>
+					<th>Header</th>
+				</tr>
+			)}
+			tbody={(
+				<>
+					<tr>
+						<th>Title</th>
+						<td>1 000</td>
+						<td>2 000</td>
+					</tr>
+					<tr>
+						<th>Title</th>
+						<td>50</td>
+						<td>100</td>
+					</tr>
+				</>
+			)}
+			tfoot={(
+				<tr>
+					<td colSpan="100%">
+						<sup>1</sup>&nbsp;
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non pretium felis.
+					</td>
+				</tr>
+			)}
 		/>
 	</div>
 );
 
-TableExample.story = {
-	name: 'Table',
-};
+export const TableExample = () => (
+	<div style={{ width: '1400px' }}>
+		<Table
+			caption="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non pretium felis. Aenean eu ipsum in magna auctor porta. Donec vestibulum nulla vel laoreet blandit."
+			thead={(
+				<>
+					<tr>
+						{/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+						<th rowSpan={2}>&nbsp;</th>
+						<th colSpan={2}>Header 1</th>
+					</tr>
+					<tr>
+						<th>First level</th>
+						<th>First level</th>
+					</tr>
+				</>
+			)}
+			tbody={(
+				<>
+					<tr>
+						<th>Title</th>
+						<td>1 000</td>
+						<td>2 000</td>
+					</tr>
+					<tr>
+						<th>Title</th>
+						<td>50</td>
+						<td>100</td>
+					</tr>
+				</>
+			)}
+			tfoot={(
+				<tr>
+					<td colSpan="100%">
+						<sup>1</sup>&nbsp;
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non pretium felis.
+					</td>
+				</tr>
+			)}
+		/>
+	</div>
+);
+
+export const WithIndentation = () => (
+	<div style={{ width: '1400px' }}>
+		<Table
+			caption="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non pretium felis. Aenean eu ipsum in magna auctor porta. Donec vestibulum nulla vel laoreet blandit."
+			thead={(
+				<>
+					<tr>
+						<th rowSpan={2}>2020</th>
+						<th colSpan={2}>Second level</th>
+					</tr>
+					<tr>
+						<th>First level</th>
+						<th>First level</th>
+					</tr>
+				</>
+			)}
+			tbody={(
+				<>
+					<tr>
+						<th className="sum">Total (with numbers)</th>
+						<td className="sum">1 000</td>
+						<td className="sum">2 000</td>
+					</tr>
+					<tr>
+						<th className="level1">First level</th>
+						<td>50</td>
+						<td>100</td>
+					</tr>
+					<tr>
+						<th className="level2">Second level</th>
+						<td>200</td>
+						<td>650</td>
+					</tr>
+					<tr>
+						<th className="level3">Third level</th>
+						<td>500</td>
+						<td>200</td>
+					</tr>
+				</>
+			)}
+			tfoot={(
+				<tr>
+					<td colSpan="100%">
+						<sup>1</sup>&nbsp;
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non pretium felis.
+					</td>
+				</tr>
+			)}
+		/>
+	</div>
+);
+
+export const NoFooter = () => (
+	<div style={{ width: '1400px' }}>
+		<Table
+			caption="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non pretium felis. Aenean eu ipsum in magna auctor porta. Donec vestibulum nulla vel laoreet blandit."
+			thead={(
+				<tr>
+					{/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+					<th>&nbsp;</th>
+					<th>Header</th>
+					<th>Header</th>
+				</tr>
+			)}
+			tbody={(
+				<>
+					<tr>
+						<th>Title</th>
+						<td>1 000</td>
+						<td>2 000</td>
+					</tr>
+					<tr>
+						<th>Title</th>
+						<td>50</td>
+						<td>100</td>
+					</tr>
+				</>
+			)}
+		/>
+	</div>
+);
