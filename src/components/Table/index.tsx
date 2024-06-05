0c76f4cdf65d6bc993c106c66fff83ac7c9bb409
johnnadeluy <johnnadel.salita@gmail.com>
2024-06-05 14:31:18 +0200

    Remove and comment out scroll functionality and add forwardRef

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,5 +1,4 @@
-import React, { forwardRef, useRef, useState, useEffect, ReactNode } from 'react';
-import { ArrowLeftCircle, ArrowRightCircle } from 'react-feather';
+import React, { forwardRef, ReactNode } from 'react';
 
 interface TableElementProps {
 	className?: string,
@@ -19,34 +18,32 @@ interface TableCellProps extends TableElementProps {
 export interface TableProps extends TableElementProps {
 	caption?: string,
 	dataNoteRefs?: string,
-	tabIndex?: number,
-	ariaLabelScrollLeftIcon?: string,
-	ariaLabelScrollRightIcon?: string,
 };
 
-export const TableHead = ({ className, children }: TableElementProps) => <thead className={className}>{children}</thead>;
+export const TableHead = forwardRef<HTMLTableSectionElement, TableElementProps>(({ className, children }, ref) => <thead className={className} ref={ref}>{children}</thead>);
 
-export const TableBody = ({ className, children }: TableElementProps) => <tbody className={className}>{children}</tbody>;
+export const TableBody = forwardRef<HTMLTableSectionElement, TableElementProps>(({ className, children }, ref) => <tbody className={className} ref={ref}>{children}</tbody>);
 
-export const TableFooter = ({ className, children }: TableElementProps) => <tfoot className={className}>{children}</tfoot>;
+export const TableFooter = forwardRef<HTMLTableSectionElement, TableElementProps>(({ className, children }, ref) => <tfoot className={className} ref={ref}>{children}</tfoot>);
 
-export const TableRow = ({ className, children }: TableElementProps) => <tr className={className}>{children}</tr>;
+export const TableRow = forwardRef<HTMLTableRowElement, TableElementProps>(({ className, children }, ref) => <tr className={className} ref={ref}>{children}</tr>);
 
-export const TableCell = ({ className, children, type, rowSpan, colSpan, scope, headers, align, level }: TableCellProps) => {
+export const TableCell = forwardRef<HTMLTableCellElement, TableCellProps>(({ className, children, type, rowSpan, colSpan, scope, headers, align, level }, ref) => {
 	const tableCellProps = {
     className: `${className ?? ''}${level ? ` level${level}`: ''}${align ? ` align-${align}` : ''}`,
     rowSpan,
     colSpan,
     scope,
     headers,
+		ref
   }
 
   if (type === 'th') return <th {...tableCellProps}>{children}</th>
   if (type === 'td') return <td {...tableCellProps}>{children}</td>
-};
+});
 
 TableCell.defaultProps = {
-  type: 'td'
+  type: "td"
 }
 
 const Table = forwardRef<HTMLTableElement, TableProps>(({
@@ -54,107 +51,17 @@ const Table = forwardRef<HTMLTableElement, TableProps>(({
 	caption,
 	dataNoteRefs,
 	children,
-	tabIndex,
-	ariaLabelScrollLeftIcon,
-	ariaLabelScrollRightIcon,
 	}, ref) => {
-	const [prevClientWidth, setPrevClientWidth] = useState(0)
-	const [hideScrollIcons, setHideScrollIcons] = useState(false)
-	const [disableLeftScrollIcon, setDisableLeftScrollIcon] = useState(false)
-	const [disableRightScrollIcon, setDisableRightScrollIcon] = useState(false)
-
-	const tableWrapperRef = useRef<HTMLDivElement>(null)
-	const captionRef = useRef<HTMLTableCaptionElement>(null)
-	const tableControlsRef = useRef<HTMLDivElement>(null)
-
-	useEffect(() => {
-		updateTableControlsDesktop()
-
-		const widthCheckInterval = setInterval(() => {
-      widthCheck()
-    }, 250)
-    window.addEventListener('resize', updateTableControlsDesktop)
-    return () => {
-      clearInterval(widthCheckInterval)
-      window.removeEventListener('resize', updateTableControlsDesktop)
-    }
-	}, []);
-
-	const widthCheck = () => {
-		if (tableWrapperRef.current?.clientWidth !== prevClientWidth) {
-      setPrevClientWidth(tableWrapperRef.current!.clientWidth)
-      updateTableControlsDesktop()
-    }
-	};
-
-	const updateTableControlsDesktop = () => {
-    if (
-      !tableWrapperRef.current ||
-      !tableControlsRef.current ||
-      !captionRef.current
-    )
-    return
-
-    const controls = tableControlsRef.current
-    const tableWrapper = tableWrapperRef.current
-    const left = controls.children.item(0)! as HTMLElement
-    const right = controls.children.item(1)! as HTMLElement
-
-    // hide controlls if there is no scrollbar
-    if (tableWrapper.scrollWidth > tableWrapper.clientWidth || tableWrapper.clientWidth === 0) {
-			setHideScrollIcons(true)
-      // disable left
-      if (tableWrapper.scrollLeft <= 0) {
-				setDisableLeftScrollIcon(true)
-      } else {
-				setDisableLeftScrollIcon(false)
-      }
-
-      // disable right
-      if (tableWrapper.scrollLeft + tableWrapper.clientWidth >= tableWrapper.scrollWidth) {
-				setDisableRightScrollIcon(true)
-      } else {
-				setDisableRightScrollIcon(false)
-      }
-
-      // move desktop controls to correct pos
-      const captionHalfHeight = captionRef.current.offsetHeight / 2
-      const controlsHalfHeight = left.scrollHeight / 2
-      left.style.marginTop = `${captionHalfHeight - controlsHalfHeight}px`
-      right.style.marginTop = `${captionHalfHeight - controlsHalfHeight}px`
-    } else {
-			setHideScrollIcons(true)
-    }
-  }; 
-
-	const scrollLeft = () => {
-		tableWrapperRef.current!.scrollLeft -= 100
-		updateTableControlsDesktop()
-	};
-	const scrollRight = () => {
-		tableWrapperRef.current!.scrollLeft += 100
-		updateTableControlsDesktop()
-	};
 
 	return (
-		<div className="ssb-table-wrapper" onScroll={updateTableControlsDesktop} ref={tableWrapperRef}>
+		<div className="ssb-table-wrapper">
 			<table className={`ssb-table${className ? ` ${className}` : ''}`} ref={ref}>
 				{caption && (
-					<caption data-noterefs={dataNoteRefs} ref={captionRef}>
+					<caption data-noterefs={dataNoteRefs}>
 						<div className="caption-wrapper">
 							<div className="caption-text-wrapper">
 								{caption}
 							</div>
-							{hideScrollIcons && (
-								<div className="scoll-icons-wrapper" ref={tableControlsRef}>
-									<button className={`scroll-icon-left${disableLeftScrollIcon ? ' disabled' : ''}`} aria-label={ariaLabelScrollLeftIcon} tabIndex={tabIndex} onClick={scrollLeft}>
-										<ArrowLeftCircle size={32} aria-hidden="true" />
-									</button>
-									<button className={`scroll-icon-right${disableRightScrollIcon ? ' disabled' : ''}`} aria-label={ariaLabelScrollRightIcon} tabIndex={tabIndex} onClick={scrollRight}>
-										<ArrowRightCircle size={32} aria-hidden="true" />
-									</button>
-								</div>
-						)}
 						</div>
 					</caption>
 				)}
@@ -163,11 +70,5 @@ const Table = forwardRef<HTMLTableElement, TableProps>(({
 		</div>
 	);
 });
-
-Table.defaultProps = {
-	tabIndex: 0,
-	ariaLabelScrollLeftIcon: 'Scroll left',
-	ariaLabelScrollRightIcon: 'Scroll right',
-};
 
 export default Table;
