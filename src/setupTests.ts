9ae2afb5e74f9fc42d830573e10ec988d7c4fbfc
Rune Johansen <runejo@gmail.com>
2024-07-09 09:01:28 +0200

    Add jest-fail-on-console and roll back to React 18.2 (#1140)
    Co-authored-by: Rune Johansen <6797636+runejo@users.noreply.github.com>

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,11 @@
 import '@testing-library/jest-dom'
+import failOnConsole from 'jest-fail-on-console'
 import util from 'util'
 
 Object.defineProperty(global, 'TextEncoder', {
   value: util.TextEncoder,
+})
+
+failOnConsole({
+  shouldFailOnWarn: true,
 })
