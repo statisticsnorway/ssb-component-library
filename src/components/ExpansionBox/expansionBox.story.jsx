f5e4d59d17150a57ad13b4b0301d5fe43e572cae
Carina <47381367+ssb-cgn@users.noreply.github.com>
2024-08-20 12:51:57 +0200

    Mim 1962 adjustment expansion box (#1243)
    * MIM-1962:Removed prop icon and added prop sparklesIcon
    
    * MIM-1962:Adjusting hovereffect and change propname
    
    * Fixed problem with scrollbar mobile
    
    * Fixes clickspace when sneakpeek and closed
    
    * MIM-1962:Bump version

--- src/components/ExpansionBox/expansionBox.story.jsx
+++ src/components/ExpansionBox/expansionBox.story.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Sun } from 'react-feather'
 import ExpansionBox from './ExpansionBox'
 
 const mockedHeader = 'This is a expansion box header'
@@ -53,28 +52,13 @@ export const DefaultOpen = () => (
 )
 
 export const SneakPeek = () => (
-  <div style={{ width: '1800px', display: 'flex', justifyContent: 'center' }}>
-    <div style={{ width: '740px', marginRight: '10px' }}>
-      <ExpansionBox
-        header='ExpansionBox with icon and sneakpeek text'
-        text={mockedText}
-        icon={<Sun size={32} />}
-        sneakPeek
-      />
-    </div>
-    <div style={{ width: '740px' }}>
-      <ExpansionBox
-        header='ExpansionBox with icon and sneakpeek content'
-        text={mockedContent}
-        icon={<Sun size={32} />}
-        sneakPeek
-      />
-    </div>
+  <div style={{ width: '740px', marginRight: '10px' }}>
+    <ExpansionBox header='ExpansionBox with sneakpeek text' text={mockedText} sneakPeek />
   </div>
 )
 
-export const Icon = () => (
-  <div style={{ width: '450px' }}>
-    <ExpansionBox header={mockedHeader} text={mockedContent} icon={<Sun size={32} />} />
+export const SneakpeekWithAiIcon = () => (
+  <div style={{ width: '740px' }}>
+    <ExpansionBox header='ExpansionBox with sneakpeek text and AI icon' text={mockedContent} aiIcon sneakPeek />
   </div>
 )
