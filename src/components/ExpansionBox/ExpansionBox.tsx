979947daf77fce9f97ff2548ecfca96d42191cef
Carina <47381367+ssb-cgn@users.noreply.github.com>
2024-08-27 12:19:24 +0200

    Mim 1968 adjust expansionbox (#1251)
    * MIM-1968:Ajust spaceing
    
    * Fixed tabbing in closed box
    
    * Fixed focus
    
    * Fixed sonarLink warnings
    
    * Fixed max-height
    
    * Remove overflow
    
    * Bump version

--- src/components/ExpansionBox/ExpansionBox.tsx
+++ src/components/ExpansionBox/ExpansionBox.tsx
@@ -12,29 +12,44 @@ export interface ExpansionBoxProps {
 }
 const ExpansionBox: React.FC<ExpansionBoxProps> = ({
   aiIcon = false,
-  className = '',
+  className,
   header = '',
   openByDefault = false,
   sneakPeek,
   text = '',
 }) => {
-  const [isOpen, toggleOpen] = useState(openByDefault)
+  const [isOpen, setIsOpen] = useState(openByDefault)
   const [maxHeight, setMaxHeight] = useState('')
   const contentRef = useRef<HTMLDivElement>(null)
+  const classNameList: string[] = [
+    'ssb-expansion-box',
+    className ?? '',
+    isOpen ? 'open' : '',
+    sneakPeek ? 'sneak-peek' : '',
+  ].filter(Boolean)
 
   useEffect(() => {
     if (contentRef.current) {
       const contentHeight = contentRef.current.scrollHeight
-      const maxHeightValue = contentHeight <= 1000 ? contentHeight + 10 : 1000
+      const maxHeightValue = contentHeight + 10
       setMaxHeight(isOpen ? `${maxHeightValue}px` : '')
+      const focusableElements = contentRef.current.querySelectorAll('button, a')
+
+      if (!isOpen) {
+        focusableElements.forEach((el) => {
+          el.setAttribute('tabindex', '-1')
+        })
+      } else {
+        focusableElements.forEach((el) => {
+          el.removeAttribute('tabindex')
+        })
+      }
     }
   }, [isOpen])
 
   return (
-    <div
-      className={`ssb-expansion-box${className ? ` ${className}` : ''}${isOpen ? ' open' : ''}${sneakPeek ? ` sneak-peek` : ''}`}
-    >
-      <button className='header' aria-expanded={isOpen ? 'true' : 'false'} onClick={() => toggleOpen(!isOpen)}>
+    <div className={classNameList.join(' ')}>
+      <button className='header' aria-expanded={isOpen ? 'true' : 'false'} onClick={() => setIsOpen(!isOpen)}>
         {aiIcon && (
           <div className='icon'>
             <SparklesIcon />
