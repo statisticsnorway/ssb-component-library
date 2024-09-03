74fafb54a8035126cf8e38d51d95d3149f38dad2
Carina <cgn@ssb.no>
2024-09-03 09:43:30 +0200

    Change warning Factbox

--- src/components/FactBox/factbox.test.jsx
+++ src/components/FactBox/factbox.test.jsx
@@ -3,6 +3,15 @@ import { screen, render } from '../../utils/test'
 import FactBox from './index'
 
 describe('FactBox component', () => {
+  let warnSpy
+
+  beforeEach(() => {
+    warnSpy = jest.spyOn(console, 'warn').mockImplementation(() => {})
+  })
+
+  afterEach(() => {
+    warnSpy.mockRestore()
+  })
   test('Matches the snapshot', () => {
     const { asFragment } = render(<FactBox header='fact box header' text='fact box Text' />)
     expect(asFragment()).toMatchSnapshot()
@@ -10,6 +19,7 @@ describe('FactBox component', () => {
   test('set openByDefault', async () => {
     const text = 'fact box Text'
     render(<FactBox header='fact box header' text={text} openByDefault />)
+    expect(warnSpy).toHaveBeenCalled()
 
     expect(await screen.findByText(text)).toBeVisible()
   })
