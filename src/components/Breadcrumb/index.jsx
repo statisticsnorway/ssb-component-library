ab4441802fd7b825dca06ee191f43e6f6853e96e
Carl-OW <142233642+Carl-OW@users.noreply.github.com>
2024-11-04 13:03:11 +0100

    767

--- src/components/Breadcrumb/index.jsx
+++ src/components/Breadcrumb/index.jsx
@@ -7,7 +7,7 @@ const Breadcrumb = ({ className, items, mobileCompressedView = false }) => {
   const [isMobile, setIsMobile] = useState(false)
 
   useEffect(() => {
-    const handleResize = () => setIsMobile(window.innerWidth < 600)
+    const handleResize = () => setIsMobile(window.innerWidth < 767)
     handleResize()
     window.addEventListener('resize', handleResize)
     return () => window.removeEventListener('resize', handleResize)
