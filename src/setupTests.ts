d907567308933059f1274e980a19dd1f0a08968b
Glenruben Larsen <gle@ssb.no>
2026-09-21 10:11:38 +0200

    Adds deny on approve-build scripts

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom'
 import failOnConsole from 'jest-fail-on-console'
-import { TextEncoder as NodeTextEncoder } from 'util'
+import { TextEncoder as NodeTextEncoder } from 'node:util'
 
 if (!globalThis.TextEncoder) {
   Object.defineProperty(globalThis, 'TextEncoder', {
